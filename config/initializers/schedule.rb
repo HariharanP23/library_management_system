ae3bb404b5cf15ead1568866e81d7347e4a602bd
HARIHARAN <hariharan.palanisamy@mallow-tech.com>
2024-01-12 10:20:13 +0530

    mailer changes

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -5,3 +5,15 @@ SCHEDULER.every '1d' do
     FacultyMailer.expiration_reminder(user).deliver_now
   end
 end
+
+SCHEDULER.every '3d' do
+  Member.where('expired_at > ?', Date.today).each do |user|
+    FacultyMailer.expiration_reminder(user).deliver_now
+  end
+end
+
+SCHEDULER.every '1d' do
+  Entry.where('due_date >= ? AND due_date >= ?', Date.yesterday, Date.today).each do |entry|
+    FacultyMailer.expiration_reminder(entry.member).deliver_now
+  end
+end
